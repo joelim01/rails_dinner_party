2d92156adc1053df8025c2de17140ffe9e7fe217
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-02 12:21:21 -0400

    DB changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902120433) do
+ActiveRecord::Schema.define(version: 20160902143426) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "commentable_id"
