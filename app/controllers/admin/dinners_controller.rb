ebf58b7c47b09266440d65d3e9948906e05051a0
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-07 16:20:44 -0400

    Tweaking dinner error messages.

--- app/controllers/admin/dinners_controller.rb
+++ app/controllers/admin/dinners_controller.rb
@@ -12,14 +12,20 @@ class Admin::DinnersController < ApplicationController
 
   def update
     @dinner = Dinner.find_by(id: params[:id])
-    @dinner.update!(dinner_params)
+    if @dinner.update(date: params[:dinner][:date], chefs: params[:dinner][:chefs])
+      @dinner.update(dishes_attributes: params[:dinner][:dishes_attributes])
       redirect_to edit_admin_dinner_path(@dinner), :flash => { :message => "Dinner updated." }
-      rescue ActiveRecord::RecordInvalid => invalid
+    else
+      render :edit
+    end
+    rescue ActiveRecord::RecordInvalid => invalid
        redirect_to edit_admin_dinner_path(@dinner), :flash => { :error => "#{invalid.record.class.name}: #{invalid.record.errors.full_messages.join " ,"}." }
   end
 
   def dinner_params
-    params.require(:dinner).permit(:date, :chefs, dishes_attributes: [:id, :name, :creators, :_destroy, ingredients_attributes: [:ingredients_string]] )
+    params.require(:dinner).permit(:date, :chefs, :dish_dropdown_id,
+              dishes_attributes: [:id, :name, :creators, :_destroy,
+                ingredients_attributes: [:ingredients_string]])
   end
 
 end
