005e37b5f592f0ccf0c66134d11b0efc1701db1e
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-09 11:10:15 -0400

    Removed reservations nested attributes, added dropdown flag for new dishes view.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,5 @@ class Reservation < ApplicationRecord
   delegate :date, :to => :dinner, :allow_nil => true
   belongs_to :user
   belongs_to :dinner
-  accepts_nested_attributes_for :dinner
-
 
 end
