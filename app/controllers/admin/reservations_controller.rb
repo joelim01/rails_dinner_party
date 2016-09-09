1da270962356b67d586c8df87982ec4c2a7bebb8
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-09 11:06:15 -0400

    Setting up controller for admin reservations

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,3 +1,32 @@
-class ReservationsController < ApplicationController
+class Admin::ReservationsController < ApplicationController
   before_filter :admin_only
+
+  def index
+    @dinners_future = Dinner.in_the_future.order('date DESC')
+    @dinners_past = Dinner.in_the_past.order('date DESC')
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to admin_reservations_path, flash: {:message => "Reservation cancelled."}
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
+      redirect_to admin_reservations_path, flash: {:message => "Reservation updated."}
+    else
+      render :edit
+    end
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:party_of, :dinner_id)
+  end
+
 end
