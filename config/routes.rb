1cc071dac489077c4dc1e15cc1db7f86e86cb2b8
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-09 15:39:49 -0400

    Pruning routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,35 @@
 Rails.application.routes.draw do
+  authenticate :user do
+    
+    resources :ingredients, only: [:create, :edit, :update]
 
-  resources :ingredients, only: [:create, :show, :edit, :update]
+    resources :dinners do
+      resources :comments, only: [:create, :edit, :update]
+      resources :reservations, only: [:create, :destroy, :new]
+    end
 
-  resources :dinners do
-    resources :images
-    resources :comments
-    resources :reservations
+    resources :dishes, only: [:index] do
+      resources :comments
+    end
+
+    resources :users, only: [:edit, :update] do
+      resources :dishes, only: [:index]
+      resources :reservations, only: [:index, :show, :create, :update]
+    end
+
+    namespace :admin do
+      # Directs /admin/products/* to Admin::ProductsController
+      # (app/controllers/admin/products_controller.rb)
+      resources :dinners
+      resources :dishes
+      resources :reservations
+      resources :users
+    end
+
+    resources :reservations, only: [:edit]
+
+
   end
-
-  resources :dishes, only: [:index, :create, :new] do
-    resources :images
-    resources :comments
-  end
-
-  post "/dishes" => redirect("admin/dishes")
-  patch "/dishes" => redirect("admin/dishes")
-
-  resources :users, only: [:edit, :update] do
-    resources :dishes, only: [:index]
-    resources :reservations
-  end
-
-  namespace :admin do
-    # Directs /admin/products/* to Admin::ProductsController
-    # (app/controllers/admin/products_controller.rb)
-    resources :dinners
-    resources :dishes
-    resources :images
-    resources :reservations
-    resources :users
-  end
-
-  resources :reservations, only: [:edit]
 
   root to: 'dinners#index'
 
