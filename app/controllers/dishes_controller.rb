214a133504393e00c858340cd186eb9e4b7d5843
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-12 18:24:46 -0400

    Removing user_id references.

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,13 +1,9 @@
 class DishesController < ApplicationController
 
   def index
-    if params[:user_id]
-      @user = User.find(params[:user_id])
-      if @user
-        @dishes = @user.dishes
-      else
-        redirect_to dinners_path(current_user), :flash => { :error => "The waiter could not find your dishes. Please try again." }
-      end
+    if current_user
+      @user = current_user
+      @dishes = @user.dishes
     else
       @dishes = Dish.all
     end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,15 +14,15 @@ class ReservationsController < ApplicationController
   end
 
   def index
-    if params[:user_id]
-      @reservations = Reservation.find_by(user_id: params[:user_id])
+    if current_user
+      @reservations = Reservation.find_by(user_id: current_user)
     else
       @reservations = Reservation.all
     end
   end
 
   def update
-    @reservation = Reservation.find_by(user_id: params[:reservation][:user_id], dinner_id: params[:reservation][:dinner_id])
+    @reservation = Reservation.find_by(user_id: current_user.id, dinner_id: params[:reservation][:dinner_id])
     if @reservation.update(party_of: params[:reservation][:party_of])
       redirect_to user_reservations_path(current_user), :flash => { :message => "Your reservation has been updated." }
     else
@@ -39,7 +39,7 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:dinner_id, :user_id, :party_of)
+    params.require(:reservation).permit(:dinner_id, :party_of)
   end
 
 end
