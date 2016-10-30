89bfb51b9aa2784b5b01e6fcfbca050c68768d0c
Joseph Lim <joseph.d.lim@gmail.com>
2016-10-30 17:11:08 -0400

    Adding Admin/Dinners dishes API

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -6,6 +6,10 @@ class DinnersController < ApplicationController
 
   def show
     @dinner = Dinner.find_by(id: params[:id])
+    respond_to do |f|
+      f.html { render :show }
+      f.json { render json: @dinner, include: 'dishes.ingredients' }
+    end
   end
 
 end
